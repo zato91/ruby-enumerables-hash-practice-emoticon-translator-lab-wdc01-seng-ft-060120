07b2ed952ee18bafb8f2b4d7c48f1a091650ed67
zara ahmadi <zatokey@gmail.com>
2020-05-18 02:46:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,13 +20,13 @@ end
 
 def get_english_meaning(link,emot_jap)
   # code goes here
-  eng_meaning = ""
+  eng_meaning = "sorry "
   yml_hash = load_library(link)
   yml_hash.each do |key,value| 
      if value[:japanese] == emot_jap
       eng_meaning = key 
       end
   end
-  eng_meaning
+   eng_meaning 
 end
 
